3f62a62e6ae186fcd4cc590015442f178567c47a
vorillaz <theodorevorillas@gmail.com>
2024-04-01 14:58:53 +0300

    fix: docker env caching API route

--- docker-nextjs-env/src/app/env/route.ts
+++ docker-nextjs-env/src/app/env/route.ts
@@ -1,9 +1,12 @@
 import { str, envsafe } from "envsafe";
+
+export const dynamic = "force-dynamic";
+
 export async function GET(request: Request) {
   const env = envsafe({
     DOCKER_MY_ENV: str(),
     DOCKER_ANOTHER_ENV: str(),
   });
 
-  return Response.json(env);
+  return Response.json({ ...env, dynamic });
 }
